cefb2fb4ab5f4e7a5c2a1aae651dcca39679ba8a
Milen-Donchev <devdonchev@gmail.com>
2023-11-16 13:43:59 +0200

    fix apos in profile select page

--- app/(netflix)/profile-select/page.tsx
+++ app/(netflix)/profile-select/page.tsx
@@ -14,7 +14,7 @@ const ProfileSelectPage = async () => {
     <div className="w-full h-full flex items-center justify-center">
       <div className="w-5/6 flex flex-col items-center gap-y-4">
         <h1 className="md:text-5xl lg:text-7xl text-2xl text-white mb-8">
-          Who's watching?
+          Who&apos;s watching?
         </h1>
         <div className="flex flex-wrap gap-x-4 md:gap-x-8">
           {profiles?.map((profile) => (
