8ff2de3795e5e350f080a0d902dd65be795003bf
Milen-Donchev <devdonchev@gmail.com>
2023-11-16 14:14:51 +0200

    change auth strategy to jwt

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -9,6 +9,7 @@ const nextAuthOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET,
   session: {
+    strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60, // 30d
     updateAge: 24 * 60 * 60, // 24h
   },
